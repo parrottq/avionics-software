7844b2125824705077f158217174e9ee8d03bdd6
Jake Bauer <jbauer@paritybit.ca>
2019-11-16 15:39:33 -0500

    Move position of test write

--- src/main.c
+++ src/main.c
@@ -481,6 +481,11 @@ int main(void)
     init_sd_card();
     // Start Watchdog Timer
     //init_wdt(GCLK_CLKCTRL_GEN_GCLK7, 14, 0);
+    // SD CARD TEST
+    uint32_t blockaddr = 0x00000000;
+    uint8_t data[] = {0x48, 0x49, 0x20, 0x53, 0x41, 0x4D, 0x0A};
+    write_block(blockaddr, data);
+    // END SD CARD TEST
 
     // Main Loop
     for (;;) {
@@ -504,13 +509,6 @@ int main(void)
 
 static void main_loop (void)
 {
-
-    // SD CARD TEST
-    /* uint32_t blockaddr = 0x00000000; */
-    /* uint8_t data[] = {0x48, 0x49, 0x20, 0x53, 0x41, 0x4D, 0x0A}; */
-    /* write_block(blockaddr, data); */
-    // END SD CARD TEST
-
     static uint32_t period = 1000;
 
     if ((millis - lastLed_g) >= period) {
