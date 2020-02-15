598dd16bcd0244d65192296db57fb4819333f3a7
Quinn Parrott <parrottq@users.noreply.github.com>
2020-02-14 23:43:37 -0500

    Added endpoint enabling/disabling and class specific setup to USB Mass Storage

---
+++ src/usb-storage.h
@@ -0,0 +1,42 @@
+/**
+ * @file usb-storage.h
+ * @desc USB Mass Storage implementation
+ * @author Quinn Parrott
+ * @date 2020-02-14
+ * Last Author:
+ * Last Edited On:
+ */
+
+#ifndef usb_storage_h
+#define usb_storage_h
+
+#include "global.h"
+
+#include "usb-standard.h"
+
+#define USB_STORAGE_ENDPOINT_SIZE 32
+
+/**
+ *  Callback to handle class specific requests.
+ *
+ *  @param packet The setup packet containing the request
+ *  @param response_length Pointer to where length of response will be placed
+ *  @param response_buffer Pointer to where response buffer will be placed
+ *
+ *  @return 0 if successful, a non-zero value otherwise
+ */
+extern uint8_t usb_storage_class_request_callback(struct usb_setup_packet *packet,
+                                                  uint16_t *response_length,
+                                                  const uint8_t **response_buffer);
+
+/**
+ *  Callback for when mass storage configuration is enabled by host.
+ */
+extern void usb_storage_enable_config_callback(void);
+
+/**
+ *  Callback for when mass storage configuration is disabled by host.
+ */
+extern void usb_storage_disable_config_callback(void);
+
+#endif /* usb_storage_h */
