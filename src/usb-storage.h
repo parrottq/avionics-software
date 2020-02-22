c3a52a65f0d2aa7d37b32cc9d298fdff50d4dba3
Quinn Parrott <parrottq@users.noreply.github.com>
2020-02-22 00:28:29 -0500

    Added a USB Mass Storage Device to usb configuration

---
+++ src/usb-storage.h
@@ -0,0 +1,20 @@
+/**
+ * @file usb-storage.h
+ * @desc USB Mass Storage implementation
+ * @author Quinn Parrott
+ * @date 2020-02-14
+ * Last Author:
+ * Last Edited On:
+ */
+
+#ifndef usb_storage_h
+#define usb_storage_h
+
+#include "global.h"
+
+#include "usb-standard.h"
+
+#define USB_STORAGE_MAX_OUT_BUFFER USB_ENDPOINT_SIZE_32
+#define USB_STORAGE_MAX_IN_BUFFER USB_ENDPOINT_SIZE_64
+
+#endif /* usb_storage_h */
