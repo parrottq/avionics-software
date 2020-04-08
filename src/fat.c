503fc23bd745f0bb17e893df1eee700f5adc9567
parrottq <parrottq@users.noreply.github.com>
2020-04-08 19:01:36 -0400

    File Allocation Table is no longer allocates itself

--- src/fat.c
+++ src/fat.c
@@ -14,7 +14,8 @@
 
 // TODO: Some of these constants should be in fat-standard.h
 /* Boot (1) + FSInfo (1) */
-#define FAT_CLUSTER_OFFSET (128 * 2)
+// TODO: Offset might need to be changed to align with clusters
+#define FAT_RESERVED_OFFSET (2)
 
 #define FAT_SECTOR_SIZE 512
 #define FAT_DIR_ENTRY_SIZE 32
@@ -26,12 +27,20 @@
 uint64_t fat_translate_sector(uint64_t block, uint64_t size, uint8_t *buffer)
 {
     // TODO: Can we clear this better, maybe the layer above handles this
-    uint8_t is_file_sector = 0;
     printf("%li\n", block);
     for (uint16_t i = 0; i < 512; i++)
     {
         buffer[i] = 0;
     }
+
+    uint8_t is_file_sector = 0;
+
+    // TODO: Document all these variables
+    uint64_t data_size_byte = size;
+    uint64_t file_size_cluster = INTEGER_DIVISION_ROUND_UP(data_size_byte, 512 * 128);
+    const uint64_t dir_size_cluster = 1;
+    uint32_t fat_size_sector = INTEGER_DIVISION_ROUND_UP(dir_size_cluster + file_size_cluster, 512 / 4);
+
     // TODO: Too much nesting break into functions
     if (block == 0)
     {
@@ -45,11 +54,12 @@ uint64_t fat_translate_sector(uint64_t block, uint64_t size, uint8_t *buffer)
         sector->BS_jmpBoot[0] = 0xeb;
         sector->BS_jmpBoot[1] = 0x58;
         sector->BS_jmpBoot[2] = 0x90;
+        const char oem_name[] = "CuInSpac";
+        memcpy(&sector->BS_OEMName, oem_name, 8);
 
         sector->BPB_BytsPerSec = FAT_SECTOR_SIZE;
         sector->BPB_SecPerClus = 1;
-        // TODO: Reserved chucks check
-        sector->BPB_RsvdSecCnt = FAT_CLUSTER_OFFSET;
+        sector->BPB_RsvdSecCnt = FAT_RESERVED_OFFSET;
         // TODO: No second fat
         sector->BPB_NumFATs = 1;
         sector->BPB_RootEntCnt = 0;
@@ -60,11 +70,10 @@ uint64_t fat_translate_sector(uint64_t block, uint64_t size, uint8_t *buffer)
         sector->BPB_SecPerTrk = 32;
         sector->BPB_NumHeads = 64;
         sector->BPB_HiddSec = 0;
-        // TODO: Set this to something
-        uint32_t total_sectors = 0;
-        sector->BPB_TotSec32 = total_sectors; // TODO: Verify that fat_get_size not what's needed
+        uint32_t total_sectors = FAT_RESERVED_OFFSET + fat_size_sector + (128 * file_size_cluster);
+        sector->BPB_TotSec32 = total_sectors;
 
-        sector->BPB_FATSz32 = INTEGER_DIVISION_ROUND_UP(total_sectors * FAT_CLUSTER_ENTRY_SIZE, FAT_SECTOR_SIZE); // TODO: total sec * 4 (32bits) / 512
+        sector->BPB_FATSz32 = fat_size_sector;
         sector->BPB_ExtFlags = 0;
         sector->BPB_FSVer = 0;
         sector->BPB_RootClus = 2; // TODO: Hard coded value
@@ -87,8 +96,6 @@ uint64_t fat_translate_sector(uint64_t block, uint64_t size, uint8_t *buffer)
         const char fat_volume_label[] = "FAT32      ";
         memcpy(sector->BS_VolLab, fat_volume_label, 11);
         memcpy(&sector->BS_FilSysType, fat_volume_label, 8);
-        const char oem_name[] = "CuInSpac";
-        memcpy(&sector->BS_OEMName, oem_name, 8);
 
         buffer[510] = 0x55;
         buffer[511] = 0xAA;
@@ -116,21 +123,14 @@ uint64_t fat_translate_sector(uint64_t block, uint64_t size, uint8_t *buffer)
     }
     // TODO: Better reserved region handling
     // Entering cluster zone
-    else if (block >= FAT_CLUSTER_OFFSET)
+    else if (block >= FAT_RESERVED_OFFSET)
     {
         printf("Dyn Section: ");
-        block -= FAT_CLUSTER_OFFSET;
-        // TODO: Document all these variables
-        uint64_t data_size_byte = size;
-        uint64_t file_size_cluster = INTEGER_DIVISION_ROUND_UP(data_size_byte, 512 * 128);
-        uint64_t dir_size_cluster = 1;
-        uint64_t cluster_count = dir_size_cluster + file_size_cluster + INTEGER_DIVISION_ROUND_UP(file_size_cluster, 128 * 512 / 4);
-        printf("Cluster count: %li\n", cluster_count);
-        uint32_t fat_size_cluster = INTEGER_DIVISION_ROUND_UP(cluster_count, 128 * 512 / 4);
-        printf("FAT cluster size: %li\n", fat_size_cluster);
+        block -= FAT_RESERVED_OFFSET;
+        printf("FAT sector size: %li\n", fat_size_sector);
         const uint64_t rollover = 3;
         // TODO: Change order so files is first checked
-        if (block < (fat_size_cluster * 128))
+        if (block < fat_size_sector)
         {
             // FAT
             printf("FAT\n");
@@ -144,32 +144,17 @@ uint64_t fat_translate_sector(uint64_t block, uint64_t size, uint8_t *buffer)
             for (uint32_t entry_offset = 0; entry_offset < 512; entry_offset += 4)
             {
                 uint32_t *entry = (uint32_t *)buffer + entry_offset;
-                if (fat_entry < fat_size_cluster)
+                if (fat_entry < dir_size_cluster)
                 {
-                    printf("FAT\n");
-                    // FAT entry
-                    if ((fat_size_cluster - 1) == fat_entry)
-                    {
-                        // End of cluster chain
-                        *entry = 0xffffffff;
-                    }
-                    else
-                    {
-                        // Point to next cluster
-                        *entry = fat_entry + 1;
-                    }
-                }
-                else if (fat_entry < (fat_size_cluster + dir_size_cluster))
-                {
-                    printf("DIR\n");
+                    printf("\tDIR entry\n");
                     // Directory entry
                     *entry = 0xffffffff;
                 }
                 else
                 {
-                    printf("File ");
+                    printf("\tFile entry: ");
                     // File entry
-                    uint64_t fat_file_entry = fat_entry - (fat_size_cluster + dir_size_cluster);
+                    uint64_t fat_file_entry = fat_entry - dir_size_cluster;
                     printf("%li\n", fat_file_entry);
                     // Rollover is the number of cluster per file
 
@@ -190,18 +175,18 @@ uint64_t fat_translate_sector(uint64_t block, uint64_t size, uint8_t *buffer)
                         *entry = fat_entry + 1;
                     }
                 }
-                printf("\tSector: %li Entry: %li Value: %li\n", block + 2, fat_entry, *entry);
+                printf("\t\tSector: %li Entry: %li Value: %li\n", block + 2, fat_entry, *entry);
                 fat_entry++;
             }
 
             // TODO: Move 128 constants somewhere
         }
-        else if (block < ((fat_size_cluster + dir_size_cluster) * 128))
+        else if (block < (fat_size_sector + (dir_size_cluster * 128)))
         {
             printf("DIR\n");
             // DIR cluster
             const uint16_t dir_entries_per_sector = (512 / 32);
-            uint32_t dir_entry = dir_entries_per_sector * (block - (fat_size_cluster * 128));
+            uint32_t dir_entry = dir_entries_per_sector * (block - fat_size_sector);
             for (uint16_t dir_entry_offset = 0; dir_entry_offset < 512; dir_entry_offset += sizeof(struct fat_directory))
             {
                 struct fat_directory *entry = (struct fat_directory *)buffer + dir_entry_offset;
