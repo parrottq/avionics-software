d4b24bb1a38596d9f17b923b126985cadb7739b6
parrottq <parrottq@users.noreply.github.com>
2020-04-11 22:01:47 -0400

    Updated test_fat.c

--- src/test-fat.c
+++ src/test-fat.c
@@ -7,25 +7,31 @@ uint8_t buffer[512];
 
 int call_all_blocks(FILE *fp)
 {
-    uint32_t size = 7;
-    //fat_get_size(callback, &size);
-    printf("Capacity %i\n", size);
+    uint64_t size = 512 * 4 * 70000;
 
-    for (uint32_t i = 0; i < size; i++)
+    uint32_t total_sectors = fat_get_total_sectors(size);
+    printf("Capacity %i Sector count %i\n", size, total_sectors);
+
+    for (uint32_t i = 0; i < total_sectors; i++)
     {
-        if (i < 100)
+        if (i < 10000 || i % 1000 == 0)
         {
-            printf("Sector %i\n", i);
+            printf("\nSector %i\n", i);
         }
-        uint64_t sector = fat_translate_sector(i, size * 512, buffer);
+
+        uint64_t sector = fat_translate_sector(i, size, buffer);
         if (sector != ~((uint64_t)0))
         {
             // Is part of the file sector is offset
-            char ch = 64; // @
+            printf("Offset: %li\n", sector);
+            char ch = 0;
             memset(buffer, ch, 512);
+            uint64_t *entry = (uint64_t *)buffer;
+            *entry = sector;
         }
 
         fwrite(buffer, sizeof(uint8_t), sizeof(buffer), fp);
+        fflush(fp);
     }
 }
 
